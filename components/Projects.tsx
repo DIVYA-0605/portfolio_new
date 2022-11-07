c84b69e1e582d3eb71a53aa0ac3f2205efe03212
DIVYA525-VAD <68990221+DIVYA525-VAD@users.noreply.github.com>
2022-11-07 16:36:24 +0530

    error cleared

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -18,14 +18,13 @@ export default function Projects({projects}: Props) {
     <div className='relative  w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20  scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 scrollbar-thin'>
 {projects?.map(
     (project,i)=>(
-<div key={project._id} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
+<div  key={i} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
 <motion.img 
 initial={{y:-300,
 opacity:0}}
 transition={{duration:1.2}}
 whileInView={{opacity:1,y:0}}
 viewport={{once:true}}
-
 src={urlFor(project?.image).url()}
 className='w-72 h-72 mt-36'
 

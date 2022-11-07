e07db6e1d436a34dc798847105338a66e0a78964
DIVYA525-VAD <68990221+DIVYA525-VAD@users.noreply.github.com>
2022-11-07 17:27:35 +0530

    i hope done

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -17,15 +17,15 @@ export default function Projects({projects}: Props) {
         <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-500 text-2xl'>Projects</h3>
     <div className='relative  w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20  scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 scrollbar-thin'>
 {projects?.map(
-    (project,i)=>(
-<div  className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
+    (project,index)=>(
+<div key={index} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
 <motion.img 
 initial={{y:-300,
 opacity:0}}
 transition={{duration:1.2}}
 whileInView={{opacity:1,y:0}}
 viewport={{once:true}}
-key={i}
+
 src={urlFor(project?.image).url()}
 className='w-72 h-72 mt-36'
 
@@ -33,7 +33,7 @@ className='w-72 h-72 mt-36'
 <div className='space-y-8 px-0 md:px-10 max-w-6xl'>
     <h4 className='text-2xl font-semibold text-center'>
         <span className='underline decoration-[#F7AB0B]/50'>
-        Case Study {i+1} of {projects.length}:
+        Case Study {index+1} of {projects.length}:
         </span>{" "}
         {project?.title}
     </h4>
